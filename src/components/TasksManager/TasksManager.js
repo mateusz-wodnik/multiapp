6c44efc8804bbcbe1e282a5c23aec428193811af
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-10-10 18:51:58 +0200

    Add basic task feature structure

---
+++ src/components/TasksManager/TasksManager.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import bs from 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './TasksManager.sass';
+
+
+const TasksManager = () => (
+    <div className={`${bs.container} ${styles.container}`}>
+        React TasksManager
+    </div>
+);
+
+export default TasksManager; // eslint-disable-line
